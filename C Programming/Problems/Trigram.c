bafa80d9f17338e3217c0fcc1d315045aff7dbb4
Dwaipayan Ray <35661311+raydwaipayan@users.noreply.github.com>
2018-07-10 22:34:52 +0530

    minor changes

--- C Programming/Problems/Trigram.c
+++ C Programming/Problems/Trigram.c
@@ -10,18 +10,22 @@ int main()
     len=strlen(a);
     register int i;
     int count=0;char *c=(char*)malloc(40*sizeof(char));int c2=0;
-    int * freq=(int*)malloc(100*sizeof(int));int cf=0;int first=0;
+    int * freq=(int*)malloc(100*sizeof(int));
+    char trigram[100][40];
+    int cf=0;int first=0;
     for(i=0;i<len;i++)
     {
         if(a[i]==' '|| a[i]=='.')
         {
+            c[c2++]=' ';
             count++;
             if(count==1)
                 first=i;
             if(count==3)
             {
                 c[c2]='\0';
-                freq[cf++]=findfreq(c);
+                freq[cf]=findfreq(c);
+                strcpy(trigram[cf++],c);
                 count=0;
                 if(i==len-2)
                     break;
@@ -36,13 +40,16 @@ int main()
         }
 
     }
-    int max=0;
+    int max=0,point=0;
     for(register int i=0;i<cf;i++)
     {
-        if(freq[i]>max)
+        if(freq[i]>max){
             max=freq[i];
+            point=i;}
     }
-    printf("Maximum freq. of trigram is %d",max);
+    printf("Maximum freq. of trigram is: %d\n",max);
+    printf("The trigram is: %s",trigram[point]);
+    return 0;
 
 }
 int findfreq(char *b)
@@ -54,6 +61,7 @@ int findfreq(char *b)
     {
         if(a[i]==' '|| a[i]=='.')
         {
+            c[c2++]=' ';
             count++;
             if(count==1)
                 first=i;
