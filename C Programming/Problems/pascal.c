c2c5075601dc84f6a6c30b47f9ee9ba651de5d2e
Dwaipayan Ray <35661311+raydwaipayan@users.noreply.github.com>
2018-07-24 23:07:10 +0530

    old algo removed

--- C Programming/Problems/pascal.c
+++ C Programming/Problems/pascal.c
@@ -4,22 +4,20 @@ int main(void)
 {
     puts("Enter the no. of lines you want to display");
     int len;scanf("%d",&len);
-    int b=4*len;
+    int b=2*len;
     if(len==0)return;
-    for(register int i=0;i<len;i++)
+    for(register int i=1;i<=len;i++)
     {
         for(register int j=0;j<b;j++)
         {
             printf(" ");
         }
-        b-=2;
-        int p=(int)pow(11,i);
-        while(p>0)
+        b=b-2;
+        int c = 1;
+        for (register int j = 1; j <= i; j++)
         {
-            int l=(int)log10(p);
-            int d=p/((int)pow(10,l));
-            printf("%4d",d);
-            p=p-d*pow(10,l);
+             printf("%3d ", c);
+             c = c * (i - j) / j;
         }
         printf("\n");
     }
